3214167d37f798c35e538e710ea0baa253b4b9bc
Peter Grassberger <petertheone@gmail.com>
2019-03-04 08:34:06 +0100

    Add map to artis: Full width, 120px height, europe.

--- src/js/directives.js
+++ src/js/directives.js
@@ -234,6 +234,15 @@ angular.module('netStatsApp.directives', [])
 					scope.map = new Datamap({
 						element: element[0],
 						scope: 'world',
+						setProjection: function(element) {
+							var projection = d3.geo.azimuthalEqualArea()
+								.center([18.55, 48.44])
+								.scale(350)
+								.translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+							var path = d3.geo.path()
+								.projection(projection);
+							return {path: path, projection: projection};
+						},
 						width: width,
 						height: 242,
 						fills: {
